129cd566363fe6e43b33b30d893973977de208db
Calin <calinoiu.alexandru@agilefreaks.com>
2016-10-09 10:19:59 +0300

    [issue-3] Add adapter for route

--- app/src/main/java/ro/gov/httpithub/infotransport/route/RouteFragment.java
+++ app/src/main/java/ro/gov/httpithub/infotransport/route/RouteFragment.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -35,10 +37,32 @@ public class RouteFragment extends Fragment implements RouteContract.View {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_route, container, false);
+        View root = inflater.inflate(R.layout.fragment_route, container, false);
+        initRecyclerView(root);
+
+        return root;
     }
 
     public void setPresenter(@NonNull RouteContract.Presenter presenter) {
         mPresenter = checkNotNull(presenter);
+    }
+
+    private void initRecyclerView(View root) {
+        RecyclerView mRecyclerView = (RecyclerView) root.findViewById(R.id.recyclerView);
+        LinearLayoutManager mLayoutManager = new LinearLayoutManager(getActivity());
+
+        int scrollPosition = 0;
+
+        if (mRecyclerView.getLayoutManager() != null) {
+            scrollPosition = ((LinearLayoutManager) mRecyclerView.getLayoutManager())
+                    .findFirstCompletelyVisibleItemPosition();
+        }
+
+        mRecyclerView.setLayoutManager(mLayoutManager);
+        mRecyclerView.scrollToPosition(scrollPosition);
+
+        String[] mDataSet = { "Stop1", "Stop2", "Stop3", "Stop4" };
+        RouteAdapter mAdapter = new RouteAdapter(mDataSet);
+        mRecyclerView.setAdapter(mAdapter);
     }
 }
